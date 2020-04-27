5387b91bcb887706da3674a9747dd3e2c831de3c
MondaleFelix <mondalefelix@yahoo.com>
2020-04-27 02:58:35 -0700

    solved second question with test cases

--- code.py
+++ code.py
@@ -40,6 +40,59 @@ two_sum([1,3,-4,6], -3)
 
 
 
+# Given a list of n strings with mixed casing, return a new list of all strings that start with a capitalized letter.
+
+# Input: [Strings]
+# Output: [Strings]
+# Test Cases:
+# Input: ["apples", "Bananas", "eggs", "Pina"]
+# Output: ["Bananas", "Pina"]
+
+
+# Create an array to store list of strings that start with capitalize letters
+# Iterate through given array of strings
+	# Check if current element starts with a capitalized letter
+		# Add current element to array
+# return created array
+
+
+def capitalized_list(arr)
+	cap_list = []
+	for word in arr:
+		if word[0].isupper():
+			cap_list.append(word)
+	return cap_list
+
+
+# Good Test Cases
+
+capitalized_list(["apples", "Bananas", "lemons", "Torta"])
+
+
+# Bad Test Cases
+
+capitalized_list(["apples", "Bananas", 9, "Torta"])
+capitalized_list(["apples", "Bananas", False, "Torta"])
+
+#Edge Cases
+
+capitalized_list(["apples", "Bananas", "      ", "Torta"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
